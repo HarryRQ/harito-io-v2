f0d346ebb5a7ed099e205cd62cfbd797e94eb855
Harito <harito@Haritos-MacBook-Pro.local>
2025-09-22 00:27:58 -0400

    Change background color and add circle for the intro

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import "../styles/Intro.css";
+
+export default function Intro() {
+  return (
+    <div className="intro">
+      <div className="intro-shapes">
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+}
