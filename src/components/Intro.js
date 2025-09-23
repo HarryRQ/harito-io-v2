6ac81ec262593490d0f4cecdf546cd5053f33dda
Harito <harito@Haritos-MacBook-Pro.local>
2025-09-22 23:32:53 -0400

    Add lines with angle and animation for circle acting like a sunshine

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,15 @@ export default function Intro() {
   return (
     <div className="intro">
       <div className="intro-shapes">
+        <div className="red-sunshine"></div>
         <div className="circle"></div>
+      </div>
+      <div className="lines">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
